345db2f4083b617276c9999ea9938ba3629b14c9
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-10 18:11:06 -0300

    feat: atualizações na div criada

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ function isCheck(index) {
   let label = check.parentElement;
   if (check.checked) {
     // label.classList.add("concluida")
-    label.classList.replace("pendente", "concluida");
+    label.classList.replace("tarefas", "concluida");
   } else {
     // label.classList.remove("concluida")
-    label.classList.replace("concluida", "pendente");
+    label.classList.replace("concluida", "tarefas");
   }
   check.addEventListener("click", contagemTarefas());
 }
@@ -69,9 +69,7 @@ function mostrarTarefas() {
     div.setAttribute("class", "tarefas");
 
     div.innerHTML = `
-    <label for="${index}" class="pendente"><input type="checkbox" id="${index}" ${
-      tarefa.checked ? "checked" : ""
-    } onclick="isCheck(${index})">${tarefa.descricao}</label>
+    <input type="checkbox" id="${index}" ${tarefa.checked ? "checked" : ""} onclick="isCheck(${index})"><label for="${index}">${tarefa.descricao}</label>
     <button class="btExcluirTarefa" onclick="excluirTarefa(${index})">Excluir</button>`;
     tarefas.appendChild(div);
   });
